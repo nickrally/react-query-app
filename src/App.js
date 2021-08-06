7a9d1228894c29c2e6f417f7d5874d853c5259c5
nickrally <nickrally2009@gmail.com>
2021-08-06 14:27:40 -0600

    switched default chart to pie

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 export default function App() {
   const options = useMemo(() => ["bar", "pie"], []);
   const [counter, setCounter] = useState(0);
-  const [selected, setSelected] = useState("bar");
+  const [selected, setSelected] = useState("pie");
   const handleSelection = (item) => {
     setSelected(item);
   };
